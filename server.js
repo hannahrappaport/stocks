7bd150f9203016b0c893e34cb47ae1ddb9407aae
hannahrappaport <51413749+hannahrappaport@users.noreply.github.com>
2025-04-22 21:09:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -50,17 +50,17 @@ http.createServer(function (req, res) {
             const results = await collection.find(search).toArray();
       
             let html = `<h2>Search Results:</h2>`;
-            //let cons = ``;
+            let cons = ``;
             if (results.length > 0) {
               results.forEach(doc => {
                 html += `<div><strong>${doc.name}</strong><br>Ticker: ${doc.ticker}<br>Price: $${doc.price}<br><br></div>`;
-                //cons += `Name: ${doc.name}, Ticker: `;
+                cons += `Name: ${doc.name}, Ticker: ${doc.ticker}, Price: $${doc.price}`;
               });
             } else {
               html += "<p>No matching results found.</p>";
             }
             
-            console.log(html);
+            console.log(cons);
             res.write(html);
             res.end();
       
